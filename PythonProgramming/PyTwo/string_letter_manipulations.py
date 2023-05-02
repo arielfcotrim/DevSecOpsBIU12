3114999c61ebda84a66296fd5ebca45fe9b2f739
arielfcotrim <arielforner@gmail.com>
2023-05-02 19:04:21 +0300

    Lesson 2 Q5 == finished

--- PythonProgramming/PyTwo/string_letter_manipulations.py
+++ PythonProgramming/PyTwo/string_letter_manipulations.py
@@ -2,57 +2,113 @@
 
 
 def print_char_index_position(string, character):
+    """
+    Prints the index position of a character in a given string.
+
+    :param string: A string.
+    :param character: A character to search for in the string.
+    :return: None.
+    """
+
     index_position_msg = f"The index position of the letter, '{character}', is: "
 
+    # Iterate over the indices of the string
     for index in range(len(string)):
+        # Check if the character at the current index is the one we are looking for
         if string[index] == character:
+            # Print the index position message followed by the current index
             print(index_position_msg + str(index))
 
 
 def get_string_before_char(string, character):
+    """
+    Returns the substring of the input string before the first occurrence of a given character.
+
+    :param string: A string.
+    :param character: A character to search for in the string.
+    :return: A string containing the substring before the first occurrence of the character.
+    """
+
+    string_before_char_msg = f"The string before the letter, '{character}', is: "
+
     partial_string = ""
-
+    # Iterate over the indices of the string
     for index in range(len(string)):
+        # Check if the character at the current index is the one we are looking for
         if string[index] == character:
+            # Stop iterating if we find the first occurrence of the character
             break
-
+        # Append the current character to the partial string
         partial_string += string[index]
 
-    return partial_string
+    return string_before_char_msg + partial_string
 
 
 def get_string_after_char(string, character):
-    reversed_string = string[::-1]
+    """
+    Returns the substring of the input string after the last occurrence of a given character.
+
+    :param string: A string.
+    :param character: A character to search for in the string.
+    :return: A string containing the substring after the last occurrence of the character.
+    """
+
+    string_after_char_msg = f"The string after the letter, '{character}', is: "
 
     partial_string = ""
-    for index in range(len(reversed_string)):
-        if reversed_string[index] == character:
+    # Iterate over the indices of the string in reverse order
+    for index in range(len(string) - 1, -1, -1):
+        # Check if the character at the current index is the one we are looking for
+        if string[index] == character:
+            # Stop iterating if we find the last occurrence of the character
             break
+        # Append the current character to the partial string
+        partial_string += string[index]
 
-        partial_string += reversed_string[index]
-
+    # Reverse the partial string to get the correct order of characters
     normalized_string = partial_string[::-1]
 
-    return normalized_string
+    return string_after_char_msg + normalized_string
 
 
 def get_string_without_char(string, character):
+    """
+    Returns the input string with all occurrences of a given character removed.
+
+    :param string: A string.
+    :param character: A character to remove from the string.
+    :return: A string containing the input string with all occurrences of the character removed.
+    """
+
+    modified_string_msg = f"The string without the letter, '{character}', is: "
+
     modified_string = ""
+    # Iterate over the characters in the string
     for letter in string:
+        # Check if the current character is not the one we are looking for
         if letter != character:
+            # Append the current character to the modified string
             modified_string += letter
 
-    return modified_string
+    return modified_string_msg + modified_string
 
 
+# Main program
 my_string = "hello world"
 char = "o"
 
-string_before_char = get_string_before_char(my_string, char)
-string_after_char = get_string_after_char(my_string, char)
-string_without_char = get_string_without_char(my_string, char)
-
+# Get the index position of each occurrence of the character
 print_char_index_position(my_string, char)
+print()
+
+# Get the string before the first occurrence of the character
+string_before_char = get_string_before_char(my_string, char)
 print(string_before_char)
+
+# Get the string after the last occurrence of the character
+string_after_char = get_string_after_char(my_string, char)
 print(string_after_char)
+
+# Get the string with all occurrences of the character removed
+string_without_char = get_string_without_char(my_string, char)
 print(string_without_char)
