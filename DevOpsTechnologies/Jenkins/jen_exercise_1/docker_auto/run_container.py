9b5e419006277c61a524d187e65bcefb79c522b2
arielfcotrim <arielforner@gmail.com>
2023-06-15 14:04:41 +0300

    quick fix in run container py

--- DevOpsTechnologies/Jenkins/jen_exercise_1/docker_auto/run_container.py
+++ DevOpsTechnologies/Jenkins/jen_exercise_1/docker_auto/run_container.py
@@ -4,4 +4,4 @@ import os
 host_port = os.environ.get('HOST_PORT')
 container_port = os.environ.get('CONTAINER_PORT')
 
-os.system(f'docker run -p {host_port}:{container_port} --name{input("Enter an image name: ")} -d {image_name}:{image_version}')
+os.system(f'docker run -p {host_port}:{container_port} --name{input("Enter a name for the container: ")} -d {image_name}:{image_version}')
